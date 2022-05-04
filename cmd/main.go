d0ba703035f69770b464b3ebd58cfca59cb21308
leighlin0511 <leighlin0511@hotmail.com>
2022-05-04 18:37:02 +1000

    cleanup

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,5 @@ import (
 )
 
 func main() {
-	// c := make(chan os.Signal, 1)
-	// signal.Notify(c, []os.Signal{os.Interrupt, syscall.SIGTERM}...)
-	// ctx, cancel := context.WithCancel(context.Background())
-	// go func() {
-	// 	osCall := <-c
-	// 	log.Printf("system call: %+v", osCall)
-	// 	cancel()
-	// }()
-
-	// if err := app.Run(ctx); err != nil {
-	// 	log.Fatal(err)
-	// }
 	app.Execute()
 }
